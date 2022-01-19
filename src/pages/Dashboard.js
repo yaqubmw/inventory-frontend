7868f6c08f2f1648d75a548e97327484c17007d6
yaqubmw <yaqubmw@gmail.com>
2022-01-19 21:15:38 +0700

    install tailwind, setting up, add Dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <p className="text-center text-indigo-500 font-black">
+        THIS IS DASHBOARD MUTHAFAKEH
+      </p>
+    </div>
+  );
+}
