6bf021252015d734c863a805671d21b19e94cfe9
yaqubmw <yaqubmw@gmail.com>
2022-01-21 19:53:47 +0700

    add some components, fix jsconfig

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "pages/Dashboard";
+
 function App() {
   return (
     <div className="App">
